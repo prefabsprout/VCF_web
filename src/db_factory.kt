c8e9ad7d347c646660be73cedc155dda49f86634
Stepan <stephenpetrosian@gmail.com>
2020-05-04 19:23:10 +0300

    Add data from source into db

--- src/db_factory.kt
+++ src/db_factory.kt
@@ -2,19 +2,20 @@ package com.vcf_web
 
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.TransactionManager
-import org.jetbrains.exposed.sql.transactions.transaction
 import java.sql.Connection
+import org.jetbrains.exposed.sql.transactions.transaction
+
 
 
 data class vcf(val id: Int, val contig: String, val left_boundary: Int,
-               val right_boundary: Int, val nucleotide: String, val rs: Int)
+               val right_boundary: Int, val nucleotide: String, val rs: String)
 object VCF_data : Table() {
     val id: Column<Int> = integer("id").autoIncrement()
     val contig: Column<String> = varchar("contig", 55)
     val left_boundary: Column<Int> = integer("left_boundary")
     val right_boundary: Column<Int> = integer("right_boundary")
-    val nucleotide: Column<String> = varchar("nucleotide", 1)
-    val rs: Column<Int> = integer("rs")
+    val nucleotide: Column<String> = varchar("nucleotide", 10000)
+    val rs: Column<String> = varchar("rs", 40)
     override val primaryKey = PrimaryKey(id)
 }
 
@@ -23,17 +24,18 @@ fun main() {
     Database.connect("jdbc:mysql://localhost:3306/vcf", driver = "com.mysql.jdbc.Driver",
             user = "newuser", password = "password")
     TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
-
     transaction {
         SchemaUtils.create(VCF_data)
-        // Test data generation
-        VCF_data.insert {
-            it[contig] = "chr2"
-            it[left_boundary] = 4
-            it[right_boundary] = 2
-            it[nucleotide] = "A"
-            it[rs] = 42
         }
-    }
-
+//    var res = transaction {
+//        VCF_data
+//                .slice(VCF_data.rs)
+//                .select { (VCF_data.contig eq "chr42" and
+//                        (VCF_data.left_boundary eq 9578804) and
+//                        (VCF_data.right_boundary eq 9578807) and
+//                        (VCF_data.nucleotide eq "A"));
+//                }
+//                .map { it[VCF_data.rs]}
+//    }
+//    println(res)
 }
